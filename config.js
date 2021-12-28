4615ab4850877cf161604ffff9537c04c281edda
coder-Sadika <89793562+coder-Sadika@users.noreply.github.com>
2021-12-28 14:32:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyBQ6i0io3tpe9VDqeSivFFlxM4EKO577GM",
   authDomain: "voting-app-d168c.firebaseapp.com",
   projectId: "voting-app-d168c",
   storageBucket: "voting-app-d168c.appspot.com",
   messagingSenderId: "443830077455",
   appId: "1:443830077455:web:d85ac811cead08c6ce7b7a"
-
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
